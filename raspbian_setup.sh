b30ea935f6de50b9ff07e13d547511747f49635a
Andreas Galazis <agalazis@gmail.com>
2014-12-26 22:13:51 +0000

    fixedthe path to rabbitmq server config file

--- raspbian_setup.sh
+++ raspbian_setup.sh
@@ -4,7 +4,7 @@ wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 sudo apt-key add rabbitmq-signing-key-public.asc
 rm rabbitmq-signing-key-public.asc
 sudo apt-get install rabbitmq-server
-sudo cp ./rabbitmq.config /etc/rabbitmq/rabbitmq.config
+sudo cp ./setupfiles/rabbitmq.config /etc/rabbitmq/rabbitmq.config
 sudo pip install pika==0.9.8
 echo "now we will test if everything works fine..."
 ./startExample
