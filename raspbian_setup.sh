1ff07842cf5c1d973f932d4d87c4e86228efe3af
Andreas Galazis <agalazis@gmail.com>
2014-12-26 22:31:49 +0000

    added server restart after configuration setup

--- raspbian_setup.sh
+++ raspbian_setup.sh
@@ -5,6 +5,7 @@ sudo apt-key add rabbitmq-signing-key-public.asc
 rm rabbitmq-signing-key-public.asc
 sudo apt-get install rabbitmq-server
 sudo cp ./setupfiles/rabbitmq.config /etc/rabbitmq/rabbitmq.config
+sudo service rabbitmq-server restart
 sudo pip install pika==0.9.8
 echo "now we will test if everything works fine..."
 ./startExample
