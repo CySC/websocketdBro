e15453710c32b8f23630df7cc1a8fda054bc0835
Andreas Galazis <agalazis@gmail.com>
2014-12-26 11:45:24 +0000

    anged the name of the setup script to denote target distro

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-sudo yum install rabbitmq-server--1.noarch.rpm
-sudo cp ./rabbitmq.config /etc/rabbitmq/rabbitmq.config
-sudo pip install pika==0.9.8
-echo "now we will test if everything works fine..."
-./startExample
